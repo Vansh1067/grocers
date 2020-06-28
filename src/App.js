d3de3fe11e96e79f55e523b3ee06134eb70b7794
Vansh Tandon <vanshtandon1067@gmail.com>
2020-06-29 02:32:13 +0530

    Add Deal and Offer to Products

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import HeaderComponent from './component/HeaderComponent/headerComponent';
 import FooterComponent from './component/FooterComponent/footerComponent';
-import SectionComponent from './containers/section'
+import SectionComponent from './containers/sectionComponent/sectionComponent'
 function App() {
   return (
     <div className="App">
--- src/containers/sectionComponent/sectionComponent.js
+++ src/containers/sectionComponent/sectionComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './sectionComponent.css'
-import Hoc from '../Hoc/hoc'
-import SliderComponent from './SliderComponent/slidercomponent';
-import CategoriesCardComponent from './categoriesCardComponent/categoriesCardComponent'
-import img from '../assest/promo.jpg';
-import SlideShow from '../assest/1.jpg';
+import Hoc from '../../Hoc/hoc'
+import SliderComponent from '../../component/SliderComponent/slidercomponent';
+import CategoriesCardComponent from '../../component/categoriesCardComponent/categoriesCardComponent'
+import img from '../../assest/promo.jpg';
+import SlideShow from '../../assest/1.jpg';
 
 const SectionComponent=(props)=>{
     return(
@@ -14,11 +14,13 @@ const SectionComponent=(props)=>{
             <CategoriesCardComponent title="Top Categories" link={[]}
              product={[{title:'Baby Care'},{title:'Banana'},{title:'Oil'},{title:'Chips'},{title:'Baby Care'},{title:'Banana'},{title:'Oil'},{title:'Chips'}]}/>
             <CategoriesCardComponent title="All Products" link={['Baby & Care','Diary & Bakery','oil & Spices','Cleaning & HouseHolds']}
-             product={[{title:'Baby Care',price:25},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5},{title:'Baby Care',price:25},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5}]}/>
+             product={[{title:'Baby Care',price:25,discPrice:30},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5},{title:'Baby Care',price:25},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5}]}/>
             <SliderComponent img={img}></SliderComponent>
 
             <CategoriesCardComponent title="Trendings Products" link={['Featured','Best Seller','Latest']}
              product={[{title:'Baby Care',price:25},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5},{title:'Baby Care',price:25},{title:'Banana',price:15},{title:'Oil',price:250},{title:'Chips',price:5}]}/>
+             <CategoriesCardComponent title="Special Offer" link={['Baby & Care','Diary & Bakery','oil & Spices','Cleaning & HouseHolds']}
+             product={[{title:'Baby Care',price:25,discPrice:30},{title:'Banana',price:15,discPrice:30},{title:'Oil',price:250,discPrice:300},{title:'Chips',price:5,discPrice:10},{title:'Baby Care',price:25,discPrice:30},{title:'Banana',price:15,discPrice:30},{title:'Oil',price:250,discPrice:280},{title:'Chips',price:5,discPrice:10}]}/>
           
         </Hoc>
     );
