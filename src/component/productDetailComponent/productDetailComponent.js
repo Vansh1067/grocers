1cbe9c1d1caaae4b1ed5d887431dcc0c9b39058e
Vansh Tandon <vanshtandon1067@gmail.com>
2020-06-30 03:14:30 +0530

    Add structure of ProductDetail Component

---
+++ src/component/productDetailComponent/productDetailComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import './productDetailComponent.css'
+import Hoc from '../../Hoc/hoc'
+const ProductDetailComponent=(props)=>{
+    return(
+        <Hoc>
+            
+        </Hoc>
+    );
+}
+export default ProductDetailComponent
