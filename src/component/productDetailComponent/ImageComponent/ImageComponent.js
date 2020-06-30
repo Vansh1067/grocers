a7818eb23f184463aa54b171b7f20827a3c09df1
Vansh Tandon <vanshtandon1067@gmail.com>
2020-06-30 05:32:31 +0530

    product detail some modification in image and details

--- src/component/productDetailComponent/ImageComponent/ImageComponent.js
+++ src/component/productDetailComponent/ImageComponent/ImageComponent.js
@@ -8,6 +8,7 @@ const ImageComponent=(props)=>{
     return(
         <Hoc class="imageContainer">
           <img src={img}/>
+          
         </Hoc>
     );
 }
--- src/component/productDetailComponent/detailsComponent/detailsComponent.js
+++ src/component/productDetailComponent/detailsComponent/detailsComponent.js
@@ -9,22 +9,17 @@ const DetailsComponent=(props)=>{
     return(
         <Hoc class="detailsContainer">
          <h1>Title</h1>
-        <div class="details">
-            <div>
-            <p>Brand : </p>
-            <span>Britania</span>
-
-            </div>
-            <div>
-            <p>Unit :</p>
-                <span>500gm</span>
-            </div>
-                
-        </div>
+        
          <div class="price">
             <h4>Product MRP : <span> $520</span> </h4>
             <h2>Selling Price :<span> $500</span></h2>
             <p>(inclusion all taxes)</p>
+         </div>
+         <div class="availabel">
+             <p>Available in :</p>
+             <span>100gm</span>
+             <span>200gm</span>
+
          </div>
          <button>Add To Cart</button>
         <div class="ad">
@@ -35,6 +30,10 @@ const DetailsComponent=(props)=>{
             </div>
             <div class="adBox">
                 <h5>Lowest price guaranteed</h5>
+                <p>Get difference refunded if you find it cheaper anywhere else.</p>
+            </div>
+            <div class="adBox">
+                <h5>Quality Assured</h5>
                 <p>Get difference refunded if you find it cheaper anywhere else.</p>
             </div>
         </div>
