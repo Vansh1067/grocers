c6e937942b4b89e8e8a19d0c71e48043b6601906
Vansh Tandon <vanshtandon1067@gmail.com>
2020-07-01 03:06:18 +0530

    add review form structure and design

--- src/component/ReviewComponent/ReviewComponent.js
+++ src/component/ReviewComponent/ReviewComponent.js
@@ -7,7 +7,18 @@ const ReviewComponent=(props)=>{
     return(
         <Hoc class="reviewContainer">
             <p>Reviews</p>
-            
+            <div class="form">
+                <h3><span>&nbsp;</span> Write Review</h3>
+                <form class="reviewForm">
+                    <label>Your Name</label>
+                    <input placeholder="Enter Your Name" type="text"></input>
+                    <label>Your Name</label>
+                    <textarea placeholder="Enter Your Review" rows="10"type="text"></textarea>
+                    <label>Rating</label>
+                    <input type="text"></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </Hoc>
     );
 }
