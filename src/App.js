534981400ba8ad8ce7824c6fa1594b13f447ad74
Vansh Tandon <vanshtandon1067@gmail.com>
2020-07-01 03:52:45 +0530

    add Backdrop

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import HeaderComponent from './component/HeaderComponent/headerComponent';
 import FooterComponent from './component/FooterComponent/footerComponent';
 import SectionComponent from './containers/sectionComponent/sectionComponent';
 import ProductListContainer from './containers/productListContainers/productListContainer'
-import ProductDetailtContainer from './containers/ProductDetailContainer/ProductDetailContainer'
+import ProductDetailtContainer from './containers/ProductDetailContainer/ProductDetailContainer';
+import Backdrop from './component/backdrop/backdrop'
 
 function App() {
   return (
     <div className="App">
+      <Backdrop/>
       <HeaderComponent></HeaderComponent>
     
     {/* <SectionComponent></SectionComponent> 
-<ProductListContainer/>*/}
+{/* <ProductListContainer/> */}
 <ProductDetailtContainer/>
       <FooterComponent></FooterComponent>
     </div>
---
+++ src/component/backdrop/backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Hoc from '../../Hoc/hoc';
+import './backdrop.css'
+const Backdrop=(props)=>{
+    return(
+        <Hoc class="backdrop">
+            
+        </Hoc>
+    );
+}
+export default Backdrop
