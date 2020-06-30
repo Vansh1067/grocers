39215138fcad61abf1b842252bf01170629e3332
Vansh Tandon <vanshtandon1067@gmail.com>
2020-07-01 04:42:03 +0530

    Add Design to login and register Component

--- src/component/accountComponent/accountComponent.js
+++ src/component/accountComponent/accountComponent.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
 import Hoc from '../../Hoc/hoc'
 import './accountComponent.css'
 const AccountComponent=(props)=>{
     return(
         <Hoc class="AccountContainer">
             <div class="acc">
-                <div class="image">
+            <FontAwesomeIcon className="accIcon"icon={faTimes}/>
+            <div class="image">
                 <p>d</p>
                 </div>
-                <div class="forms">
-                    <p>j</p>
+                <div class="form">
+                <h3>Register Now !</h3>
+                <form class="reviewForm">
+                    <label>Email/Mobile Number</label>
+                    <input placeholder="Enter Your Name" type="text"></input>
+                    <label>Password</label>
+                    <input type="text"></input>
+                    <label>Confirm Password</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                    <button type="submit">Create Your Account</button>
+                    <div id="checkbox">
+                    <input type="checkbox" /><p> I Agree with Term &amp; Condition</p>
+
+                    </div>
+                </form>
+                <div class="buttons">
+                    <button>Login</button>
+                    <button>Register</button>
+
                 </div>
+            </div>
             </div>
         </Hoc>
     );
