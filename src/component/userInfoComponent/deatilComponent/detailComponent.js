dc1f3b95281126bbfca0ea9d8599c8c371d67b9e
Vansh Tandon <vanshtandon1067@gmail.com>
2020-07-02 03:19:19 +0530

    add design to details component in userinfo component

--- src/component/userInfoComponent/deatilComponent/detailComponent.js
+++ src/component/userInfoComponent/deatilComponent/detailComponent.js
@@ -6,7 +6,49 @@ import './detailComponent.css'
 const DetailComponent=(props)=>{
     return(
         <Hoc class="details">
-            
+            <div class="form">
+                <h3>My Profile</h3>
+                <form class="reviewForm">
+                    <div>
+                    <label>First Name</label>
+                    <input placeholder="Enter Your Name" type="text"></input>
+                    </div>
+                 <div>
+                 <label>Last Name</label>
+                    <input type="text"></input>
+                 </div>
+                   <div>
+                   <label>Phone</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div>
+                   <div>
+                   <label>Email Address</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div><div>
+                   <label>Country</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div><div>
+                   <label>City</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div><div>
+                   <label>Zip Code</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div><div>
+                   <label>State</label>
+                    <input placeholder="Give your review a title " type="text"></input>
+                   </div><div class="textarea">
+                   <label>Address</label>
+                    <textarea placeholder="Give your review a title " rows={5} type="text"></textarea>
+                   </div>
+                   <div id="btn">
+                   <button type="submit">Cancel</button>
+                    <button type="submit">Save Changes</button>
+                   </div>
+                    
+
+                  
+                </form>
+            </div>
         </Hoc>
     );
 }
